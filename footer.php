8ba86315e9968011df980da14e81c8f23b99995b
George D. Vasquez II <george.nelsoft@gmail.com>
2017-05-01 19:54:57 +0800

    Initial Commit
    -Add Bootstrap template: Light Bootstrap Dashboard
    -Add Highcharts source folder
    -Add three initial pages in navigation
    -Add header.php and footer.php mechanism

---
+++ footer.php
@@ -0,0 +1,40 @@
+        <footer class="footer">
+            <div class="container-fluid">
+                <p class="copyright pull-right">
+                    &copy; <script>document.write(new Date().getFullYear())</script> <a href="https://www.facebook.com/georgevasquezii">George Vasquez II</a>, made with love for a better Mantis Solution
+                </p>
+            </div>
+        </footer>
+
+    </div>
+</div>
+
+
+</body>
+
+    <!--   Core JS Files   -->
+    <script src="assets/js/jquery-1.10.2.js" type="text/javascript"></script>
+    <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+    <!--  Checkbox, Radio & Switch Plugins -->
+    <script src="assets/js/bootstrap-checkbox-radio-switch.js"></script>
+
+    <!--  Charts Plugin -->
+    <script src="assets/js/chartist.min.js"></script>
+
+    <!--  Notifications Plugin    -->
+    <script src="assets/js/bootstrap-notify.js"></script>
+
+    <!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
+    <script src="assets/js/light-bootstrap-dashboard.js"></script>
+
+    <!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
+    <script src="assets/js/demo.js"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function(){
+            demo.initChartist();
+        });
+    </script>
+
+</html>
